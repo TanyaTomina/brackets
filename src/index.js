e8c308320498e41dd49f5f7cc9bbadb7ba178e23
Tanya Tomina <tanya.tomina@fl3xx.com>
2020-03-21 22:25:33 +0300

    feat: add brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const open_array = [];
+    const close_array = [];
+    let stack_open = [];
+
+    for(let j = 0; j < bracketsConfig.length; j++) {
+        open_array.push(bracketsConfig[j][0]);
+        close_array.push(bracketsConfig[j][1]);
+    }
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < open_array.length; j++) {
+            if (str[i] === open_array[j] && str[i] === close_array[j]) {
+                if (stack_open[stack_open.length - 1] === j) {
+                    stack_open.pop();
+                } else {
+                    stack_open.push(j);
+                }
+            } else if (str[i] === open_array[j]) {
+                stack_open.push(j);
+            } else if (str[i] === close_array[j]) {
+                if (stack_open.pop() === j) {
+                } else {
+                    return  false;
+                }
+            }
+        }
+    }
+    if (stack_open.length) {
+        return false;
+    } else {
+        return true;
+    }
 }
